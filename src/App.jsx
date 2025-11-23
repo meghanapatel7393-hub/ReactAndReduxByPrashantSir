5a8863608ea24598ec6137f7d1f0c9d603031131
Meghana <meghanapatel7393@gmail.com>
2025-11-23 14:03:06 +0530

    add new learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItem from "./components/TodoItem";
+import "./TodoApp.css"; //this is used for Todo
 import Hello from "./Hello";
 import KgButton from "./KgButton";
 import Random from "./Random";
 
 //this is called functional component
-function App(){
-return (
-  <>
-  <h1>Hello World</h1>
-  <KgButton/>
-  <Hello/>
-  <Random/>
-  <Random/>
-  <Random/>
-  </>
-);
+function App() {
+  /*this is for todo app*/
+  return (
+    <>
+      <center>
+        <AppName />
+        <AddTodo />
+        <div className="items-container">
+          <TodoItem />
+        </div>
+      </center>
+    </>
+  );
+  /*this is basic working during learning 
+  return (
+    <>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
+      <h1>Hello World</h1>
+      <KgButton />
+      <Hello />
+      <Random />
+      <Random />
+      <Random />
+    </>
+  );
+  */
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
-import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+// import App from "./App.jsx";
+import BharatClock from "./BharatClock.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BharatClock />
+    {/* <App /> */}
+  </StrictMode>
+);
